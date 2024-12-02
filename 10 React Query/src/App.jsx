68c67570b0c60c5141885b811d0b2bbfafd7cb5f
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-02 12:50:53 +0500

    react-query and tailwind install

--- 10 React Query/src/App.jsx
+++ 10 React Query/src/App.jsx
@@ -6,6 +6,9 @@ import './App.css'
 function App() {
   return (
     <>
+      <div className='flex justify-center items-center h-dvh'>
+        <h1 className='text-3xl font-medium'>Hello World</h1>
+      </div>
     </>
   )
 }
